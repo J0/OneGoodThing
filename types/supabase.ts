622e4b964275cb96e287df07c0b7580471938792
Terry Sutton <saltcod@gmail.com>
2023-03-13 14:13:34 +0800

    Add types and client

---
+++ types/supabase.ts
@@ -0,0 +1,196 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      deeds: {
+        Row: {
+          created_at: string
+          desc: string | null
+          id: string
+          public: boolean | null
+        }
+        Insert: {
+          created_at: string
+          desc?: string | null
+          id?: string
+          public?: boolean | null
+        }
+        Update: {
+          created_at?: string
+          desc?: string | null
+          id?: string
+          public?: boolean | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      http: {
+        Args: {
+          request: Database["public"]["CompositeTypes"]["http_request"]
+        }
+        Returns: Database["public"]["CompositeTypes"]["http_response"]
+      }
+      http_delete:
+        | {
+            Args: {
+              uri: string
+            }
+            Returns: Database["public"]["CompositeTypes"]["http_response"]
+          }
+        | {
+            Args: {
+              uri: string
+              content: string
+              content_type: string
+            }
+            Returns: Database["public"]["CompositeTypes"]["http_response"]
+          }
+      http_get:
+        | {
+            Args: {
+              uri: string
+            }
+            Returns: Database["public"]["CompositeTypes"]["http_response"]
+          }
+        | {
+            Args: {
+              uri: string
+              data: Json
+            }
+            Returns: Database["public"]["CompositeTypes"]["http_response"]
+          }
+      http_head: {
+        Args: {
+          uri: string
+        }
+        Returns: Database["public"]["CompositeTypes"]["http_response"]
+      }
+      http_header: {
+        Args: {
+          field: string
+          value: string
+        }
+        Returns: Database["public"]["CompositeTypes"]["http_header"]
+      }
+      http_list_curlopt: {
+        Args: Record<PropertyKey, never>
+        Returns: {
+          curlopt: string
+          value: string
+        }[]
+      }
+      http_patch: {
+        Args: {
+          uri: string
+          content: string
+          content_type: string
+        }
+        Returns: Database["public"]["CompositeTypes"]["http_response"]
+      }
+      http_post:
+        | {
+            Args: {
+              uri: string
+              content: string
+              content_type: string
+            }
+            Returns: Database["public"]["CompositeTypes"]["http_response"]
+          }
+        | {
+            Args: {
+              uri: string
+              data: Json
+            }
+            Returns: Database["public"]["CompositeTypes"]["http_response"]
+          }
+      http_put: {
+        Args: {
+          uri: string
+          content: string
+          content_type: string
+        }
+        Returns: Database["public"]["CompositeTypes"]["http_response"]
+      }
+      http_reset_curlopt: {
+        Args: Record<PropertyKey, never>
+        Returns: boolean
+      }
+      http_set_curlopt: {
+        Args: {
+          curlopt: string
+          value: string
+        }
+        Returns: boolean
+      }
+      is_json: {
+        Args: {
+          "": string
+        }
+        Returns: boolean
+      }
+      send_email_mailgun: {
+        Args: {
+          message: Json
+        }
+        Returns: Json
+      }
+      send_email_message: {
+        Args: {
+          message: Json
+        }
+        Returns: Json
+      }
+      urlencode:
+        | {
+            Args: {
+              string: string
+            }
+            Returns: string
+          }
+        | {
+            Args: {
+              string: string
+            }
+            Returns: string
+          }
+        | {
+            Args: {
+              data: Json
+            }
+            Returns: string
+          }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      http_header: {
+        field: string
+        value: string
+      }
+      http_request: {
+        method: unknown
+        uri: string
+        headers: unknown
+        content_type: string
+        content: string
+      }
+      http_response: {
+        status: number
+        content_type: string
+        headers: unknown
+        content: string
+      }
+    }
+  }
+}
